524f8635cd145e0cbadbd90c4c668acf4520e6d9
Riddhesh Sanghvi <riddhesh.sanghvi@rtcamp.com>
2018-03-08 11:17:59 +0530

    Fix interactive command issue

--- src/WP_Command.php
+++ src/WP_Command.php
@@ -14,6 +14,7 @@
 class WP_Command extends EE_Command {
 
 	private $sites;
+
 	/**
 	 * Executes wp-cli command on a site.
 	 *
@@ -24,40 +25,40 @@ class WP_Command extends EE_Command {
 	 *
 	 */
 	public function __invoke( $args, $assoc_args ) {
-		// print_r($args);
 		$this->load_sites_from_db();
 
 		$site_name = $args[0];
 
 		if ( in_array( $site_name, $this->sites ) ) {
-			$wp_command = 'wp ' . implode( ' ', array_slice( $args, 1 ) ) ;
-			$docker_compose_command = 'docker-compose exec --user=www-data php ' . $wp_command;
-			$site_dir = EE::get_runner()->config['sites_path'] . '/' . $site_name ;
-			$site_src_dir = $site_dir . '/app/src' ;
 
-			chdir( $site_dir );
-
-			$process = \EE::launch( $docker_compose_command, false, true );
-
-			// Check if user is running `wp db export`
-			if( !empty( $args[1] ) && $args[1] === 'db' && !empty( $args[2] ) && $args[2] === 'export' ) {
-				$export_file_name = !empty( $args[3] ) ? $args[3] : '' ;
-
-				// If export file name is `-`, then wp-cli will redirect to STDOUT.
-				if( empty( $export_file_name ) || !empty( $export_file_name ) && $export_file_name !== '-' ) {
-					\EE::log( "You can find your exported file in $site_src_dir" );
+			$arguments = '';
+			if ( ! empty( $assoc_args ) ) {
+				foreach ( $assoc_args as $key => $value ) {
+					$arguments .= ' --' . $key . '=' . $value;
 				}
 			}
 
-			if( !empty( $process->stdout ) ) {
-				\EE::log( $process->stdout );
-			}
-			if( !empty( $process->stderr ) ) {
-				\EE::log( $process->stderr );
-			}
-		}
+			$wp_command             = 'wp ' . implode( ' ', array_slice( $args, 1 ) ) . $arguments;
+			$docker_compose_command = 'docker-compose exec --user=www-data php ' . $wp_command;
+			$site_dir               = EE::get_runner()->config['sites_path'] . '/' . $site_name;
+			$site_src_dir           = $site_dir . '/app/src';
 
-		else {
+			chdir( $site_dir );
+
+			$process = passthru( $docker_compose_command, $return );
+
+			EE::log( print_r( $return, true ) );
+
+			// Check if user is running `wp db export`
+			if ( ! empty( $args[1] ) && $args[1] === 'db' && ! empty( $args[2] ) && $args[2] === 'export' ) {
+				$export_file_name = ! empty( $args[3] ) ? $args[3] : '';
+
+				// If export file name is `-`, then wp-cli will redirect to STDOUT.
+				if ( empty( $export_file_name ) || ! empty( $export_file_name ) && $export_file_name !== '-' ) {
+					\EE::log( "You can find your exported file in $site_src_dir" );
+				}
+			}
+		} else {
 			EE::error( "No site with name `$site_name` found." );
 		}
 	}
@@ -68,13 +69,14 @@ class WP_Command extends EE_Command {
 	private function load_sites_from_db() {
 
 		$this->sites = [];
-		$runner = EE::get_runner();
+		$runner      = EE::get_runner();
 
-		$dir = dir($runner->config['sites_path']);
+		$dir = dir( $runner->config['sites_path'] );
 
-		while (false !== ($entry = $dir->read())) {
-			if( $entry !== '.' && $entry !== '..' )
-			array_push( $this->sites, $entry );
+		while ( false !== ( $entry = $dir->read() ) ) {
+			if ( $entry !== '.' && $entry !== '..' ) {
+				array_push( $this->sites, $entry );
+			}
 		}
 	}
 }
